e7e6be33fa2cb8ae86ff0934ddd7e3a6e6d22867
mmekhanov <mikhail.mekhanov@eurochem.ru>
2016-02-02 15:28:48 +0300

    Version 0.1.2

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,5 +2,5 @@ class nginx::params {
   $package_ensure = 'present'
   $service_manage = true
   $service_ensure = 'running'
-  $www_root_path  = '/var/www/noodoo/current'
+  $www_root_path  = '/var/www/noodoo/app'
 }
