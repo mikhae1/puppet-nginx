67c1743c31115f3a6445ffbc884de3e8ba9fabbe
mmekhanov <mikhail.mekhanov@eurochem.ru>
2016-02-03 11:49:57 +0300

    Add firewall unless

--- manifests/config.pp
+++ manifests/config.pp
@@ -24,12 +24,13 @@ class nginx::config inherits nginx {
   }
 
   exec {'firewall-add-80-tcp':
-    command => "/usr/bin/firewall-cmd --permanent --add-port=80/tcp",
-    #require => Package["sphinx"],
+    command => '/usr/bin/firewall-cmd --permanent --add-port=80/tcp',
+    unless => '/usr/bin/firewall-cmd --list-all | /bin/grep 80/tcp'
   }
 
   exec {'firewall-reload':
     command => '/usr/bin/firewall-cmd --reload',
-    require => Exec['firewall-add-80-tcp'],
+    subscribe => Exec['firewall-add-80-tcp'],
+    refreshonly => true, 
   }
 }
